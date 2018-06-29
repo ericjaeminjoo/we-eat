45348a8fbea9e75d4590cdeb865115cdef5fbd16
Sylvain Goedike <sgoedike@videotron.ca>
2018-06-29 00:22:06 +0000

    Created dynamic DOM element for dish-modal

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,7 +9,7 @@
 //   });;
 // });
 $(document).ready(function() {
-  // Creates a DOM for a dish
+  // Creates a DOM element for a dish
   function createDish(dish) {
     return  `
       <a href="#" id="${dish.id}" class="food-item list-group-flush list-group-item-action menu-item border-top" data-toggle="modal" data-target="#exampleModalCenter">
@@ -22,6 +22,39 @@ $(document).ready(function() {
           <i class="fal fa-plus-square fa-lg pl-2"></i>
         </div>
       </a>
+    `
+  };
+  // Creates a DOM element for a single dish in modal
+  function createDishModal(dish) {
+    return  `
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div id= "dish" class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">
+              ${dish.name}
+            </h5>
+            <a class="close" data-dismiss="modal" aria-label="Close">
+              <i class="fal fa-times-circle"></i>
+            </a>
+          </div>
+          <div class="modal-body">
+            ${dish.description}
+          </div>
+          <div class="modal-footer d-flex justify-content-between">
+            <div>
+              <button id="decrease-quantity">
+                <i class="fal fa-minus"></i>
+              </button>
+              <input type="text" id="quantity-value" value="1" class="text-center"></input>
+              <button id="increase-quantity">
+                <i class="fal fa-plus"></i>
+              </button>
+            </div>
+            <button type="button" class="btn btn-primary" id="add-to-cart">ADD TO CART
+            </button>
+          </div>
+        </div>
+      </div>
     `
   };
 
@@ -55,24 +88,49 @@ $(document).ready(function() {
     loadFoodCategory(foodCategoryEl, foodCategoryObj[foodCategoryEl]);
   };
 
+  // Renders a single dish into index.html
+  const renderSingleDishModal = (foodArr) => {
+    for (let foodItem of foodArr) {
+      $('.modal')
+        .append(createDishModal(foodItem));
+    }
+  };
+
   // Gets the dish object by its id when clicked
   $('#menu-container').on('click', '.food-item', function (event) {
     $.ajax({
       method: "GET",
       url: `/dish/${this.id}`
     }).done(results => {
-      renderSingleDish(results);
+      renderSingleDishModal(results);
     }).catch(err => {
       console.log(err);
     })
   });
 
-  // Renders a single dish into index.html
-  const renderSingleDish = (foodArr) => {
-    for (let foodItem of foodArr) {
-      $('#dish')
-        .append(createDish(foodItem));
+  // Decreases qty of single dish on modal
+  $("#decrease-quantity").click(function() {
+    let quantity = $("#quantity-value").val();
+    if ($("#quantity-value").val() <= 0) {
+      $("#decrease-quantity").attr("disabled", "disabled");
+    } else {
+      $("#quantity-value").attr("value", --quantity);
     }
-  };
+  });
+
+  // Increases qty of single dish on modal
+  $("#increase-quantity").click(function () {
+    let quantity = $("#quantity-value").val();
+    console.log(quantity);
+    if ($("#quantity-value").val() >= 0) {
+      $("#decrease-quantity").removeAttr("disabled");
+    }
+    $("#quantity-value").attr("value", ++quantity);
+  });
+
+  // Gets the dish object by its id when clicked
+  $('.modal').on('click', '.close', function (event) {
+    $('.modal-dialog').remove();
+  });
 
 });
