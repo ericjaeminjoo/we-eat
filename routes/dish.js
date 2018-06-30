831925f5927d7d7db34804a40ddd26dfedc2ffcc
Olivier Martin <olimartin90@gmail.com>
2018-06-30 02:21:03 +0000

    creating router.post for /dish_order and /order and test

--- routes/dish.js
+++ routes/dish.js
@@ -65,10 +65,10 @@ module.exports = knex => {
 
   // Gets the order from db
   router.get("/order", (req, res) => {
-    const dish_order_id = req.params.id;
+    // const dish_order_id = req.params.id;
     knex("dish_order")
-      .select("qty", "special_inst", "dish_id", "order_id", )
-      .where({id: dish_order_id})
+      .select("qty", "special_inst", "dish_id", "order_id")
+      .where({ dish_order })
       .then(result => {
         res.json(result);
       })
@@ -79,5 +79,55 @@ module.exports = knex => {
   });
 
 
+  // Post the final cart for checkout
+  router.post("/dish_order", (req, res) => {
+
+    const cartObj = req.body.cart;
+
+    for (let cartElement of cartObj) {
+      knex('dish_order')
+        .insert([{id: cartElement.id, name: cartElement.name, description: cartElement.description, price: cartElement.price, qty: cartElement.qty}])
+        .select('*')
+        .catch(error => {
+          console.log("Error: ", error);
+          return Promise.resolve();
+        })
+        // .finally(() => {
+        //   knex.destroy();
+        // })
+    }
+  });
+
+  
+  // Post the final checkout
+  router.post("/order", (req, res) => {
+
+    knex('order')
+      .insert([{id: req.body.id, phone: req.body.phone, time_stamp: req.body.time_stamp, time_pickup: req.body.time_pickup}])
+      .select('*')
+      .catch(error => {
+        console.log("Error: ", error);
+        return Promise.resolve();
+      })
+      // .finally(() => {
+      //   knex.destroy();
+      // })
+  }); 
+
   return router;
 };
+
+// const param1 = process.argv[2];
+// const param2 = process.argv[3];
+// const param3 = process.argv[4];
+
+// knex('famous_people')
+//   .insert([{first_name: param1, last_name: param2, birthdate: param3}])
+//   .select('*')
+//   .catch(function(error) {
+//     console.error(error)
+//   })
+//   .finally(() => {
+//     console.log("Query is completed!");
+//     knex.destroy();
+//   });
